5aa16463cbdb3563342ea17c06097c0d3aea4d98
Mahin Alam <mahinalam351@gmail.com>
2025-12-15 21:41:03 +0600

    added second question

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ Event Loop: Handles asynchronous tasks (I/O, timers, requests) without blocking 
 Libuv library: Provides a thread pool and handles background tasks like file system operations and networking.
 Non-blocking I/O: Lets Node.js process thousands of concurrent requests efficiently without creating multiple threads
    
+// 2. How Does Node.js Handle Multiple Tasks While Being Single-Threaded?
+Node.js একটি event-driven এবং non-blocking I/O architecture অনুসরণ করে। যদিও JavaScript কোড single thread-এ চলে, কিন্তু সময়সাপেক্ষ বা ভারী কাজগুলো Node.js নিজে execute করে না।
+এই ধরনের কাজগুলো Node.js Operating System-এর kernel অথবা thread pool-এ পাঠিয়ে দেয়। এই সময় Node.js-এর main thread অন্য কাজ বা request handle করতে থাকে।
+যখন সেই ভারী কাজগুলো সম্পন্ন হয়, তখন তাদের callback বা result Event Loop-এর queue-তে যোগ হয়। Event Loop তখন call stack ফাঁকা পেলেই সেই callback গুলো execute করে।
+এই পদ্ধতির মাধ্যমে Node.js:
+নতুন thread তৈরি না করেও একসাথে অনেক request efficiently handle করতে পারে
